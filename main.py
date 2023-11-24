70536b84b65e0f81eb2547359c7190d7ed3bda58
Steven Hall <73695945+bryanb619@users.noreply.github.com>
2023-11-24 23:52:23 +0000

    First commit

---
+++ main.py
@@ -0,0 +1,36 @@
+from p5 import *
+
+# window size
+window_width = 1200
+window_height = 800
+
+
+def setup():
+    size(window_width, window_height)
+    title("Steven Hall Exercise 2")
+
+    """ Function that draws a car
+
+    """
+def draw_car():
+    pass
+
+def draw_buildings():
+    box(40,170,70)
+
+def background_():
+    pass
+
+
+def draw():
+    background(0)
+    box(70,170,70)
+
+
+
+
+
+
+
+
+run(mode='P3D')
